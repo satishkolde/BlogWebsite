295dce93f8951c02f44ecc0aa48d8351a674923a
satishkolde <satishkolde335@gmail.com>
2026-05-09 00:09:17 +0530

    Updated Blog routes

--- api/src/routes/blog.route.js
+++ api/src/routes/blog.route.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
-import { createBlogController, deleteBlogController, getAllBlogsController, getPaginatedBlogsController, getSpecificBlogController, getTotalNumberOfPagesController, getUserPrivateBlogsController, getUserPublicBlogController, updateBlogController } from "../controllers/blog.controler.js";
+import { createBlogController, deleteBlogController, getAllBlogsController, getPaginatedBlogsController, getSpecificBlogController, getUserPrivateBlogsController, getUserPublicBlogController, updateBlogController } from "../controllers/blog.controler.js";
 
 const route = Router();
 
@@ -8,11 +8,10 @@ route.use(verifyJwt);
 route.route("/").get(getAllBlogsController);
 route.route("/me").get(getUserPrivateBlogsController);
 route.route("/pagination").get(getPaginatedBlogsController);
-route.route("/pagination/pages").get(getTotalNumberOfPagesController);
 route.route("/:username").get(getUserPublicBlogController);
 route.route("/specific").post(getSpecificBlogController);
 route.route("/").post(createBlogController);
-route.route("/").delete(deleteBlogController);
-route.route("/").patch(updateBlogController);
+route.route("/:id").delete(deleteBlogController);
+route.route("/:id").patch(updateBlogController);
 
 export default route;
